f6f5fd181a745a35a6e798df0a6090f8118102e2
timeyy44 <2072482130@qq.com>
2019-12-19 20:44:12 +0800

    AR小游戏

---
+++ hw11/3DProject_AR/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vbb)
+    {
+        ani.SetTrigger("Run");
+        Debug.Log("按钮按下");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vbb)
+    {
+        ani.SetTrigger("Walk");
+        Debug.Log("按钮释放");
+    }
+}
